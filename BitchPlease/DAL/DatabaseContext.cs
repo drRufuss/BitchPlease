c3912d05abea2b736d6af0258f3abbd6d559646e
Rufuss <rufhs1@gmail.com>
2020-04-19 20:04:17 +0200

    Prepare solution

---
+++ BitchPlease/DAL/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BitchPlease.Models;
+
+namespace BitchPlease.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) 
+            : base(options)
+        {
+        }
+    }
+}
--- BitchPlease/Startup.cs
+++ BitchPlease/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using BitchPlease.DAL;
 
 namespace BitchPlease
 {
@@ -25,6 +27,9 @@ namespace BitchPlease
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DatabaseContext>(opt => 
+                opt.UseNpgsql(Configuration.GetConnectionString("BitchPleaseDatabase")));
+
             services.AddControllers();
         }
 
